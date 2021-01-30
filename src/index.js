98c0f45f95e6c51738d982ed2f9d885c7de3795d
vakxaminas <sverxkabzdec2019@gmail.com>
2021-01-30 21:31:30 +0300

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+exports.min = function min(array) {
+    if (array !== undefined && array.length) return Math.min.apply(null, array);
+    else return 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array !== undefined && array.length) return Math.max.apply(null, array);
+    else return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array !== undefined && array.length)
+        return array.reduce((sum, current) => sum + current, 0) / array.length;
+    else return 0;
+};
